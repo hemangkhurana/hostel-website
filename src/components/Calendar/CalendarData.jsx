8175607e7aa4cd85284e26c2477492355c873210
Hemang Khurana <hemangkhurana@gmail.com>
2021-09-10 03:02:38 +0530

    another image-carousel

---
+++ src/components/Calendar/CalendarData.jsx
@@ -0,0 +1,16 @@
+const CalendarData = [
+    {
+        image: "https://images.unsplash.com/photo-1630763626005-c687551f6d4e?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1630936404616-df78789005c1?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1630795014794-3a12e1b4c839?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1630829958815-d7e1d0f337b2?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"
+    }
+]
+
+export default CalendarData;
--- src/components/image-slider/ImageSlider.jsx
+++ src/components/image-slider/ImageSlider.jsx
@@ -1,36 +1,41 @@
 import React from "react";
+import styles from "./ImageSlider.module.css";
 import SliderData from "./SliderData";
-import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from "react-icons/fa";
-import styles from  "./ImageSlider.module.css";
 
-function ImageSlider({slides}) {
-    const [current, setCurrent] =React.useState(0)
-    const length = slides.length
-
-    function nextSlide() {
-        setCurrent(current===length-1 ? 0 : current+1)
+function ImgComp({src}) {
+    let imgStyles={
+        width:100+"%",
+        height: "auto"
     }
-    function prevSlide() {
-        setCurrent(current===0? length-1 : current-1)
-    }
-    console.log(current);
-    
+}
 
-    if ( !Array.isArray(slides) || slides.length <= 0 ) {
-        return null;
+function ImageSlider() {
+
+    const [x, setX] = React.useState(0)
+
+    function goLeft() {
+        return (
+            x === 0?setX(-100*(SliderData.length-1)):setX(x+100)
+        );
+    }
+
+    function goRight() {
+        return (
+            x === -100*(SliderData.length-1)?setX(0):setX(x-100)
+        );
     }
 
     return (
         <div className={styles.slider}>
-            <FaArrowAltCircleLeft className={styles.left_arrow} onClick={prevSlide}/>
-            <FaArrowAltCircleRight className={styles.right_arrow} onClick={nextSlide}/>
-            {SliderData.map(function(obj, index) {
-                return (
-                    <div className={index===current ? 'obj active' : 'obj'} key={index}>
-                        {index===current && (<img className={styles.image} src={obj.image} alt="Image" />)}
+            {SliderData.map(function(item, index) {
+                return(
+                    <div key={index} className={styles.slide} style={{transform:`translateX(${x}%)`}}>
+                        {<img className={styles.img} src={item.image} alt="image"/>}
                     </div>
                 );
             })}
+            <button id={styles.goLeft} onClick={goLeft}>left</button>
+            <button id={styles.goRight} onClick={goRight}>right</button>
         </div>
     );
 }
