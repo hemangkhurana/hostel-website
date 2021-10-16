c20f9a0aaaa46321c8d1b623d3e1a15e2bd87f41
hemangkhurana <73101506+hemangkhurana@users.noreply.github.com>
2021-10-16 18:39:02 +0530

    Revert "changed layout of team page"

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -10,19 +10,16 @@ function Team() {
         <div>
             <h1 className={styles.heading1}>Our Team</h1>
             <hr className={styles.hr1}/>
-
             <h2 className={styles.heading2}>Secretary</h2>
             <div className={styles.Team_container}>
                 {SecteraryData.map(TeamCard)}
             </div>
             <hr className={styles.hr2}/>
-
             <h2 className={styles.heading2}>Representatives</h2>
             <div className={styles.Team_container}>
                 {RepresentativeData.map(TeamCard)}
             </div>
             <hr className={styles.hr2}/>
-            
             <h2 className={styles.heading2}>Captian and Vice-Captian</h2>
             <div className={styles.Team_container}>
                 {CaptianAndViceCaptianData.map(TeamCard)}
