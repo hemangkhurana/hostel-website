0a70455a2d345f8251938bd6250db96ed1901bbd
hemangkhurana <hemangkhurana@gmail.com>
2021-10-16 18:08:22 +0530

    changed layout of team page

--- src/components/Team/TeamCard.jsx
+++ src/components/Team/TeamCard.jsx
@@ -5,18 +5,13 @@ import styles from "./Team.module.css";
 function TeamCard(props) {
     return(
         <div className={styles.TeamCard}>
-            <div className={styles.TeamCard_top}>
-                <h1 className={styles.name}>{props.name}</h1>
-                <h2 className={styles.position}>{props.position}</h2>
-            </div>
-            <div className={styles.TeamCard_mid}>
-                <p className={styles.description}>{props.description}</p>
-            </div>
-            <div className={styles.TeamCard_bottom}>
-                <a className={styles.phone} href="tel: {props.phone}">{props.phone}</a>
-                <a className={styles.mail} href="mailto: {props.mail}">{props.mail}</a>
-            </div>
             <img src={props.src} alt="PersonImage" className={styles.Card_img}/>
+            <h1 className={styles.name}>{props.name}</h1>
+            <h2 className={styles.position}>{props.position}</h2>
+            <div className={styles.contact_details}>
+                <a className={styles.phone} href="tel: {props.phone}"><i class="fas fa-phone-square-alt"></i></a>
+                <a className={styles.mail} href="mailto: {props.mail}"><i class="fas fa-envelope-square"></i></a>
+            </div>
         </div>
     );
 }
