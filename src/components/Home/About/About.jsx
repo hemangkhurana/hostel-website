8b62beee90387370ec65d19cce431a1dda449ce5
Shresth Ojha <shresthojha@Shresths-MacBook-Pro.local>
2022-09-09 20:14:41 +0530

    major changes in home page (navbar and minor bugs)

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -12,10 +12,10 @@ function About() {
             />
             <div className={styles.about_body}>
                 <h1 className={styles.heading}>About Us</h1>
-                {/* <hr className={styles.horizontal_line} /> */}
+                <hr className={styles.horizontal_line} />
                 <p className={styles.about_description}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Ut enim ad minim veniam, quis nostrud exercitation fsdklfsdjf sdf d fskj fdslk dsfkjlsd ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </p>
             </div>
         </div>
