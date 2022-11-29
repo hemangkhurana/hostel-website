e03bbfaeefe7f7e0ff9e197c148a8b51c7b5113b
Shresth-Ojha <ojhashresth@gmail.com>
2022-11-30 00:46:47 +0530

    fixed navbar dropdown, added navbar.css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import "./navbar.css"
 import {Link} from "react-router-dom";
 import pic from './logo-removedbg.png';
 
@@ -11,7 +12,7 @@ function Navbar() {
         return setToggleMenu(!toggleMenu);
     }
 
-    const navbarClass = toggleMenu ? 'active' : '';
+    const navbarClass = toggleMenu ? 'navbarLOLactive' : '';
 
     return (
         <div className={styles.navbar}>
@@ -21,7 +22,8 @@ function Navbar() {
                 <h1 className={styles.navbar_heading}>NILGIRI</h1>
             </Link>
             
-            <ul className={`${styles.navbar_menu} ${styles.navbarClass}`}>
+            <ul className={`${styles.navbar_menu} ${navbarClass}`}>
+            {/* <ul className="navbar_menu navbarLOLactive"> */}
                 <Link className={styles.react_router_link_item} to="/">
                     <li className={styles.navbar_item} onClick={ChangeToggleMenu}>
                         <p className={styles.navbar_itemlol}>Home</p>
