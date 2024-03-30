02be2d2aa70cce686d4af14fa4debc27b1e5b439
Shresth-Ojha <shresthojha.iitdelhi@gmail.com>
2024-03-30 20:45:25 +0530

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./components/App.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import App from './components/App.jsx';
+import { Analytics } from '@vercel/analytics/react';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App/>
-)
+    <>
+        <App />
+        <Analytics />
+    </>
+);
